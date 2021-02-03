f6e8f792cbf1b7b8767f9c573388eccd7eb82e5b
Peter MkII <gilconcepcionjr@gmail.com>
2021-02-03 14:00:24 +0800

    Exam Files

---
+++ wp-content/themes/gil-exam/header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+
+<title>Gil Concepcion Jr</title></title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" media="all"/>.
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<?php wp_head(); ?>
+</head>
+<body>
+
+
+<div class="head-one text-center ">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark  static-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+              <img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="">
+            </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                    <span class="sr-only">(current)</span>
+                  </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Navigation -->
+
+
+    <div class="container fill ">
+        <div class="row">
+            <div class="container-lg">
+              <h1>Hi there! We are the new kids on the block <br>
+                    and we build awesome websites and mobile apps.</h1>
+                <a href="#" class="btn btn-gradient orange large">WORK WITH US</a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ wp-content/themes/gil-exam/index.php
@@ -0,0 +1,328 @@
+<?php get_header(); ?>
+
+
+<!-- SERVICES-->
+<div class="services">
+    <div class="container">
+        <div class="row pad-45">
+            <div class="col-3">&nbsp;</div>
+            <div class="col-6">
+                <h2 class="text-center svcs">SERVICES WE PROVIDE</h2>
+                <hr>
+                <p class="text-xl-center font-weight-bold white">We are working with both individuals and businesses from all over the globe to create awesome websites and applications.</p>
+            </div>
+            <div class="col-3">&nbsp;</div>
+        </div>
+
+        <div class="row pad-45">
+            <div class="col-3">
+                <div class="text-center">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/flag.png">
+                    <h3 class="text-center white">BRANDING</h3>
+                    <p class="text-xl-center white">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.</p>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="text-center">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/pencil.png">
+                    <h3 class="text-center white">DESIGN</h3>
+                    <p class="text-xl-center white">Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
+                </div>
+            </div>
+            
+            <div class="col-3">
+                <div class="text-center">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/gears.png">
+                    <h3 class="text-center white">DEVELOPMENT</h3>
+                    <p class="text-xl-center white">At vero eos et accusamus et iusto odio dignissimos qui blanditiis praesentium.</p>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="text-center">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/rocket.png">
+                    <h3 class="text-center white">ROCKET SCIENCE</h3>
+                    <p class="text-xl-center white">Et harum quidem rerum est et expedita distinctio. Nam libero tempore.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<!-- TEAM-->
+<div class="team">
+    <div class="container">
+        <div class="row pad-45">
+            <div class="col-2">&nbsp;</div>
+            <div class="col-8">
+                <h2 class="text-center lb-blue">MEET OUR BEAUTIFUL TEAM</h2>
+                <hr>
+                <p class="text-xl-center font-weight-bold lb-blue">We are a small team of designers and developers, who help brands with big ideas.
+                </p>
+            </div>
+            <div class="col-2">&nbsp;</div>
+        </div>
+
+        <div class="row pad-45">
+            <div class="col-3">
+                <div class="text-center">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/mug.png">
+                    <h3 class="text-center db-blue">ANNE HATHAWAY</h3>
+                    <h4 class="text-center lt-blue">CEO/ Marketing Guru</h4>
+                    <p class="text-xl-center lb-blue">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna.</p>
+                        <a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-google"></a>
+                        <a href="#" class="fa fa-linkedin"></a>
+                    </div>
+            </div>
+
+            <div class="col-3">
+                <div class="text-center">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/mug.png">
+                    <h3 class="text-center db-blue">KATE UPTON</h3>
+                    <h4 class="text-center lt-blue">Creative Director</h4>
+                    <p class="text-xl-center lb-blue">Duis aute irure dolor in in voluptate velit esse cillum dolore fugiat nulla pariatur. Excepteur sint occaecat non diam proident.</p>
+                        <a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-google"></a>
+                        <a href="#" class="fa fa-linkedin"></a>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="text-center">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/mug.png">
+                    <h3 class="text-center db-blue">OLIVIA WILDE</h3>
+                    <h4 class="text-center lt-blue">Lead Designer</h4>
+                    <p class="text-xl-center lb-blue">Nemo enim ipsam voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem.</p>
+                        <a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-google"></a>
+                        <a href="#" class="fa fa-linkedin"></a>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="text-center">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/mug.png">
+                    <h3 class="text-center db-blue">ASHLEY GREENE</h3>
+                    <h4 class="text-center lt-blue">SEO/ Developer</h4>
+                    <p class="text-xl-center lb-blue">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                        <a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-google"></a>
+                        <a href="#" class="fa fa-linkedin"></a>
+                </div>
+            </div>
+         </div>
+    </div>
+</div>
+
+<!-- WE GOT SKILLS-->
+<div class="skills">
+    <div class="container">
+        <div class="row pad-45">
+            <div class="col-3">&nbsp;</div>
+            <div class="col-6">
+                <h2 class="text-center lb-blue">WE GOT SKILLS</h2>
+                <hr>
+                <p class="text-xl-center font-weight-bold lb-blue">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+            </div>
+            <div class="col-3">&nbsp;</div>
+        </div>
+
+        <div class="row pad-45">
+            <div class="col-3">                     
+                <div class="text-center">
+                      <img src="<?php bloginfo('template_directory'); ?>/img/90per.png">
+                      <p>&nbsp;</p>
+                      <h3 class="text-center db-blue">WEB DESIGN</h3>                      
+                </div>
+            </div> 
+
+            <div class="col-3">                     
+                <div class="text-center">
+                      <img src="<?php bloginfo('template_directory'); ?>/img/75per.png">
+                      <p>&nbsp;</p>
+                      <h3 class="text-center db-blue">HTML/CSS</h3>                      
+                </div>
+            </div> 
+
+            <div class="col-3">                     
+                <div class="text-center">
+                      <img src="<?php bloginfo('template_directory'); ?>/img/90per.png">
+                      <p>&nbsp;</p>
+                      <h3 class="text-center db-blue">GRAPHIC DESIGN</h3>                      
+                </div>
+            </div> 
+
+            <div class="col-3">                     
+                <div class="text-center">
+                      <img src="<?php bloginfo('template_directory'); ?>/img/85per.png">
+                      <p>&nbsp;</p>
+                      <h3 class="text-center db-blue">UI/ UX</h3>                      
+                </div>
+            </div>           
+         </div>
+    </div>
+</div>
+
+
+<!-- OUR PORTFOLIO -->
+<div class="portfolio">
+    <div class="container">
+        <div class="row pad-45">
+            <div class="col-3">&nbsp;</div>
+            <div class="col-6">
+                <h2 class="text-center dk-gray">OUR PORTFOLIO</h2>
+                <hr>
+                <p class="text-xl-center font-weight-bold dk-gray">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet consectetur, adipisci velit, sed quia non numquam </p>
+            </div>
+            <div class="col-3">&nbsp;</div>
+        </div>
+
+        <div class="row pad-45">
+            <div class="col-6">                     
+                <div class="text-center">
+                      <img class="reduced" src="<?php bloginfo('template_directory'); ?>/img/portfolio-1.png">
+                      <p>&nbsp;</p>
+                      <h3 class="text-center dk-gray all-cp">Isometric Pearspective Mock-Up</h3>                      
+                </div>
+            </div> 
+            
+            <div class="col-6">                     
+                <div class="text-center">
+                      <img class="reduced"  src="<?php bloginfo('template_directory'); ?>/img/portfolio-2.png">
+                      <p>&nbsp;</p>
+                      <h3 class="text-center dk-gray all-cp">Time Zone App UI</h3>                      
+                </div>
+            </div>  
+         </div>
+
+        <div class="row pad-45">
+            <div class="col-6">                     
+                <div class="text-center">
+                      <img class="reduced" src="<?php bloginfo('template_directory'); ?>/img/portfolio-3.png">
+                      <p>&nbsp;</p>
+                      <h3 class="text-center dk-gray all-cp">VIRO MEDIA PLAYERS UI</h3>                      
+                </div>
+            </div> 
+            
+            <div class="col-6">                     
+                <div class="text-center">
+                      <img class="reduced"  src="<?php bloginfo('template_directory'); ?>/img/portfolio-4.png">
+                      <p>&nbsp;</p>
+                      <h3 class="text-center dk-gray all-cp">BLOG/MAGAZINE FLAT UI KIT</h3>                      
+                </div>
+            </div>  
+         </div>
+         <div class="row pad-45 ">
+             <div class="col-12"> 
+                <div class="text-center">
+                    <a href="#" class="btn btn-gradient green large">LOAD MORE PROJECTS</a> 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- WHAT PEOPLE SAY ABOUT US-->
+<div class="whatpeople">
+    <div class="container">
+        <div class="row pad-45">
+            <div class="col-2">&nbsp;</div>
+            <div class="col-8">
+                <h2 class="text-center white">WHAT PEOPLE SAY ABOUT US</h2>
+                <hr>
+                <p class="text-xl-center font-weight-bold white">Our clients love us! </p>
+            </div>
+            <div class="col-2">&nbsp;</div>
+        </div>
+
+        <div class="row pad-45">
+            <div class="col-1">                     
+                <div class="text-center">
+                      <img class="reduced" src="<?php bloginfo('template_directory'); ?>/img/mug.png">                                           
+                </div>
+            </div> 
+
+            <div class="col-5">                     
+                <div>
+                      <h4 class="white testi"><i>“Nullam dapibus blandit orci, viverra gravida dui lobortis eget. Maecenas fringilla urna eu nisl scelerisque.”</i></h4>
+                      <h4 class="font-weight-bold  white">CHANEL IMAN</h4>
+                      <h5 class="flesh">CEO of Pinterest</h5>                      
+                </div>
+            </div>         
+
+            <div class="col-1">                     
+                <div class="text-center">
+                      <img class="reduced" src="<?php bloginfo('template_directory'); ?>/img/mug.png">                                           
+                </div>
+            </div> 
+
+            <div class="col-5">                     
+                <div>
+                      <h4 class="white testi"><i>“Nullam dapibus blandit orci, viverra gravida dui lobortis eget. Maecenas fringilla urna eu nisl scelerisque.”</i></h4>
+                      <h4 class="font-weight-bold  white">CHANEL IMAN</h4>
+                      <h5 class="flesh">CEO of Pinterest</h5>                      
+                </div>
+            </div>         
+                        
+
+         </div>
+
+<div class="row pad-45">
+            <div class="col-1">                     
+                <div class="text-center">
+                      <img class="reduced" src="<?php bloginfo('template_directory'); ?>/img/mug.png">                                           
+                </div>
+            </div> 
+
+            <div class="col-5">                     
+                <div>
+                      <h4 class="white testi"><i>“Nullam dapibus blandit orci, viverra gravida dui lobortis eget. Maecenas fringilla urna eu nisl scelerisque.”</i></h4>
+                      <h4 class="font-weight-bold  white">CHANEL IMAN</h4>
+                      <h5 class="flesh">CEO of Pinterest</h5>                      
+                </div>
+            </div>         
+
+            <div class="col-1">                     
+                <div class="text-center">
+                      <img class="reduced" src="<?php bloginfo('template_directory'); ?>/img/mug.png">                                           
+                </div>
+            </div> 
+
+            <div class="col-5">                     
+                <div>
+                      <h4 class="white testi"><i>“Nullam dapibus blandit orci, viverra gravida dui lobortis eget. Maecenas fringilla urna eu nisl scelerisque.”</i></h4>
+                      <h4 class="font-weight-bold  white">CHANEL IMAN</h4>
+                      <h5 class="flesh">CEO of Pinterest</h5>                      
+                </div>
+            </div>         
+                        
+
+         </div>
+
+    </div>
+</div>
+
+<!--GET IN TOUCH-->
+<div class="getintouch">
+    <div class="container">
+        <div class="row pad-45">
+            <div class="col-1">&nbsp;</div>
+            <div class="col-10">
+                <h2 class="text-center white">GET IN TOUCH</h2>
+                <hr>
+                <p class="text-xl-center font-weight-bold white">1600 Pennsylvania Ave NW, Washington, DC 20500, United States of America. Tel: (202) 456-1111</p>
+            </div>
+            <div class="col-1">&nbsp;</div>
+        </div>
+
+    </div>
+</div>
+<?php get_footer(); ?>
